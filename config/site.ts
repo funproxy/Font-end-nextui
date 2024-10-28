13567e91bac4d7e2c85012e88feccd96d0a08f72
wikig <carzyg@outlook.com>
2024-10-28 05:57:09 +0000

    init the page

---
+++ config/site.ts
@@ -0,0 +1,25 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Funproxy . Best Blinks for meme",
+  description: "First blinks referral service for pump meme .",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "https://docs.funproxy.site/",
+    },
+  ],
+  navMenuItems: [
+  ],
+  links: {
+    github: "https://github.com/funproxy",
+    twitter: "https://x.com/funproxys",
+    docs: "https://docs.funproxy.site",
+    discord: "https://discord.gg/MBD6bRWVzg",
+    sponsor: "https://pump.fun/Eq1Wrk62j2F2tLf9XfdBssYJVr5k8oLJx3pqEL1rpump",
+  },
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Funproxy font-end 
+
+This repo is to storage the base font-end of [funproxy](https://funproxy.site/)
+
+Including functions :
+
+- Connect wallet and get user public key
+
+- Generate referral link for memes
+
+More details please visit [Generator](https://x.com/funproxys/status/1850204801965125964)
+
+If you wants to support us : [pump us](https://pump.fun/Eq1Wrk62j2F2tLf9XfdBssYJVr5k8oLJx3pqEL1rpump)
